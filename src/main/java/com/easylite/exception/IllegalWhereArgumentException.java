326b368b331afead2a4259b2f9b4de3f9d8504b6
Mario Dennis <mdennis876@gmail.com>
2015-03-12 22:56:27 -0800

    Add IllegalWhereArgumentException

---
+++ src/main/java/com/easylite/exception/IllegalWhereArgumentException.java
@@ -0,0 +1,16 @@
+package com.easylite.exception;
+
+public class IllegalWhereArgumentException extends IllegalArgumentException {
+	private static final long serialVersionUID = -830242728952167825L;
+	
+	public IllegalWhereArgumentException() {}
+	
+	public IllegalWhereArgumentException (IllegalArgumentException t){
+		super(t);
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Invalid WhereClause or WhereArgs supplied";
+	}
+}
